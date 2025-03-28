5f5c10687d2430b43697bb23464ad992d2bbd980
Daniel <a01567295@tec.mx>
2025-03-28 16:11:12 +0000

    commit con bulma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+// import CSS
+import 'bulma/css/bulma.css'
+
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
+
+import jinWoo from './imagenes/JinWoo.png'
+import huTao from './imagenes/HuTao.png'
+import Luffy from './imagenes/Luffy.png'
 
 export function App() {
   return (
     <div>
         <div>Besto caracters segun 僕</div>
-        <Tarjeta nombre = "Sung Jin-woo" color = "negro"/>
-        <Tarjeta nombre = "Hu Tao" color = "rojo"/>
-        <Tarjeta nombre = "pendiente" color = "pendiente"/>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-2">
+                <Tarjeta nombre="Sung Jin-woo" color="negro" imagen={jinWoo}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="HuTao" color="rojo" imagen={huTao}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Luffy" color="paja" imagen={Luffy}/>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
+  
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -5,6 +5,7 @@ export function Tarjeta(props) {
     <div>
       <div>Nombre del personaje: {props.nombre}</div>
       <div>Color del personaje: {props.color}</div>
+      <img src = {props.imagen} width = {150}/>
     </div>
   )
 }
